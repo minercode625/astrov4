f3f78e4164c42dd3bb9ea349d6a6c6fd8cecf381
minercode625 <seowangduk@gmail.com>
2024-05-05 15:55:24 +0900

    Obsidian, Markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,8 +1,16 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-  ft = { "markdown" },
-  event = "BufRead",
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+    event = "BufRead",
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function() require("render-markdown").setup {} end,
+  },
 }
