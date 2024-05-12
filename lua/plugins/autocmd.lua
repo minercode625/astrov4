28ae6f75b7f0bbae24bbdab3edd9b52fb933b564
minercode625 <seowangduk@gmail.com>
2024-05-13 02:05:06 +0900

    grammarly lua file

--- lua/plugins/autocmd.lua
+++ lua/plugins/autocmd.lua
@@ -13,12 +13,4 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal tabstop=4 shiftwidth=4 expandtab",
 })
 
-require("lspconfig").grammarly.setup {
-  on_attach = function(client, bufnr)
-    -- LaTeX 파일에만 Grammarly LSP 활성화
-    if vim.bo[bufnr].filetype == "tex" then client.resolved_capabilities.document_formatting = true end
-  end,
-  filetypes = { "markdown", "tex", "text" }, -- LaTeX 파일에서 Grammarly LSP를 활성화
-  -- 추가 설정이 필요한 경우 여기에 작성
-}
 return {}
